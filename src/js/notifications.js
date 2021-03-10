917e4b8ff1d17fbec7a166a037ae97ca3f0aa9f7
alla karpenko <karpenkoallo4ka@gmail.com>
2021-03-10 12:50:13 +0200

    update

--- src/js/notifications.js
+++ src/js/notifications.js
@@ -1,6 +1,6 @@
 
- import { alert, defaultModules } from 'node_modules/@pnotify/core/dist/PNotify.js';
-  import * as PNotifyMobile from 'node_modules/@pnotify/mobile/dist/PNotifyMobile.js';
+ import { alert, defaultModules } from 'pnotify/core/dist/PNotify.js';
+  import * as PNotifyMobile from 'pnotify/mobile/dist/PNotifyMobile.js';
  
   defaultModules.set(PNotifyMobile, {});
  
