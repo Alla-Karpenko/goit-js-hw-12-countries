48a8ea63ba1692e378d57e147c2017811aec492a
alla karpenko <karpenkoallo4ka@gmail.com>
2021-03-10 12:46:35 +0200

    добавляем фалы

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import './styles.css';
+import './js/fetchCountries';
+import './js/notifications';
+
---
+++ src/js/notifications.js
@@ -0,0 +1,12 @@
+
+ import { alert, defaultModules } from 'node_modules/@pnotify/core/dist/PNotify.js';
+  import * as PNotifyMobile from 'node_modules/@pnotify/mobile/dist/PNotifyMobile.js';
+ 
+  defaultModules.set(PNotifyMobile, {});
+ 
+  alert({
+    text: 'Too many matches found.'
+});
+
+
+
